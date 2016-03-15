1788bddd6e0af8634bd2f1f75f4270886f45772a
Michael Greene <mgreenegit@users.noreply.github.com>
2016-03-15 16:03:48 -0500

    Update agent install product identifier

--- cMMAgent/DSCResources/cMMAgentInstall/cMMAgentInstall.schema.psm1
+++ cMMAgent/DSCResources/cMMAgentInstall/cMMAgentInstall.schema.psm1
@@ -15,7 +15,7 @@
     )
     
     if ($Ensure -eq 'Absent') {
-        $CommandLineArguments = '/Q /C:"MsiExec.exe /x{EB03FA06-01A7-49F7-8BD0-0AB92D905899} /qn /norestart"'
+        $CommandLineArguments = '/Q /C:"MsiExec.exe /x{E854571C-3C01-4128-99B8-52512F44E5E9} /qn /norestart"'
         Package MMAgentSetup {
             Name = 'Microsoft Monitoring Agent'
             ProductId = 'EB03FA06-01A7-49F7-8BD0-0AB92D905899'
@@ -30,7 +30,7 @@
 
         Package MMAgentSetup {
             Name = 'Microsoft Monitoring Agent'
-            ProductId = 'EB03FA06-01A7-49F7-8BD0-0AB92D905899'
+            ProductId = 'E854571C-3C01-4128-99B8-52512F44E5E9'
             Path = $Path
             Arguments = $CommandLineArguments
             Ensure = 'Present'
